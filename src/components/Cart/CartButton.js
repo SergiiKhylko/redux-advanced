c05cfa5e638e704139fd4e05c5a3390bf5bc8d8a
Sergii <sergii.khylko@outlook.com>
2023-03-27 14:11:54 +0300

    #1 create main slice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
 import styles from "./CartButton.module.css";
+import {useDispatch} from "react-redux";
+import {mainActions} from "../../store/main-slice";
 
 const CartButton = props => {
+
+  const dispatcher = useDispatch();
+
+  const cartVisibilityHandler = () => {
+    dispatcher(mainActions.toggleCartVisibility());
+  };
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={cartVisibilityHandler} >
       <span>Cart</span>
       <span className={styles.badge}>2</span>
     </button>
